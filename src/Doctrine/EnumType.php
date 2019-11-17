add0f42b4f41dd38b72a36a21c0ccac7e4a34438
Konstantin Grachev <me@grachevko.ru>
2019-11-17 22:02:29 +0300

    [Feature] Add EnumType to easy registering types in Doctrine DBAL

---
+++ src/Doctrine/EnumType.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Premier\Enum\Doctrine;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use Premier\Enum\Enum;
+
+/**
+ * @author Konstantin Grachev <me@grachevko.ru>
+ */
+final class EnumType extends Type
+{
+    /**
+     * @var string
+     */
+    private $class;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $property;
+
+    /**
+     * @psalm-param class-string<Enum> $class
+     */
+    public static function register(string $class, string $name, string $property = 'id'): void
+    {
+        if (!\is_subclass_of($class, Enum::class, true)) {
+            throw new \InvalidArgumentException(\sprintf('%s is not child of %s', $class, Enum::class));
+        }
+
+        Type::addType($name, self::class);
+        $type = Type::getType($name);
+        \assert($type instanceof self);
+
+        $type->class = $class;
+        $type->name = $name;
+        $type->property = $property;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        if ('id' === $this->property) {
+            return $platform->getSmallIntTypeDeclarationSQL($fieldDeclaration);
+        }
+
+        return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?Enum
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        $class = $this->class;
+        if ($value instanceof $class) {
+            \assert($value instanceof Enum);
+
+            return $value;
+        }
+
+        if ('id' === $this->property) {
+            if (false === $id = \filter_var($value, FILTER_VALIDATE_INT)) {
+                throw ConversionException::conversionFailed($value, $this->getName());
+            }
+
+            $value = $id;
+        }
+
+        /** @var callable $callable */
+        $callable = [$class, 'from'];
+        $enum = $callable($this->property, $value);
+
+        if (!$enum instanceof $class) {
+            throw ConversionException::conversionFailed($value, $this->getName());
+        }
+
+        \assert($enum instanceof Enum);
+
+        return $enum;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        $class = $this->class;
+        if (!$value instanceof $class) {
+            throw ConversionException::conversionFailed($value, $this->getName());
+        }
+
+        \assert($value instanceof Enum);
+
+        return $value->get($this->property);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return true;
+    }
+}
---
+++ tests/Doctrine/EnumTypeTest.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Premier\Enum\Tests\Doctrine;
+
+use Doctrine\DBAL\Platforms\SQLAnywherePlatform;
+use Doctrine\DBAL\Types\Type;
+use PHPUnit\Framework\TestCase;
+use Premier\Enum\Doctrine\EnumType;
+use Premier\Enum\Enum;
+use Premier\Enum\Tests\TestEnum;
+
+/**
+ * @author Konstantin Grachev <me@grachevko.ru>
+ */
+final class EnumTypeTest extends TestCase
+{
+    /**
+     * @var EnumTestPlatform
+     */
+    private static $platform;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$platform = new EnumTestPlatform();
+    }
+
+    public function testRegister(): void
+    {
+        EnumType::register(TestEnum::class, 'test_enum');
+
+        self::assertInstanceOf(EnumType::class, Type::getType('test_enum'));
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @dataProvider conversionDataProvider
+     */
+    public function testConversion(Type $type, Enum $enum, $value): void
+    {
+        self::assertSame($value, $type->convertToDatabaseValue($enum, self::$platform));
+        self::assertSame($enum, $type->convertToPHPValue($value, self::$platform));
+    }
+
+    public function conversionDataProvider(): \Generator
+    {
+        yield [$this->getType('test_int_enum'), TestEnum::one(), 1];
+        yield [$this->getType('test_int_enum'), TestEnum::two(), 2];
+
+        yield [$this->getType('test_string_enum', 'identifier'), TestEnum::one(), 'uno'];
+        yield [$this->getType('test_string_enum'), TestEnum::two(), 'duo'];
+    }
+
+    /**
+     * @dataProvider declarationDataProvider
+     */
+    public function testDeclaration(EnumType $type, string $declaration): void
+    {
+        self::assertSame($declaration, $type->getSQLDeclaration([], self::$platform));
+    }
+
+    public function declarationDataProvider(): \Generator
+    {
+        yield [$this->getType('test_int_enum'), 'smallint'];
+        yield [$this->getType('test_string_enum', 'identifier'), 'varchar'];
+    }
+
+    private function getType(string $name, string $property = 'id'): Type
+    {
+        if (!Type::hasType($name)) {
+            EnumType::register(TestEnum::class, $name, $property);
+        }
+
+        return Type::getType($name);
+    }
+}
+
+class EnumTestPlatform extends SQLAnywherePlatform
+{
+    public function getVarcharTypeDeclarationSQL(array $field): string
+    {
+        return 'varchar';
+    }
+
+    public function getSmallIntTypeDeclarationSQL(array $columnDef): string
+    {
+        return 'smallint';
+    }
+}
