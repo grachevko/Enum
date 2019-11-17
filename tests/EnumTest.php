b32054c0ebe0195960896d743fa087a9b64027b6
Konstantin Grachev <me@grachevko.ru>
2019-11-17 22:01:48 +0300

    Extract TestEnum class to separate file

--- tests/EnumTest.php
+++ tests/EnumTest.php
@@ -114,52 +114,6 @@ final class EnumTest extends TestCase
 }
 
 /**
- * @method static TestEnum one()
- * @method static TestEnum two()
- * @method string getDescription()
- * @method string getDescriptionTwo()
- * @method static self fromName(string $name)
- * @method static self undefinedMethod()
- */
-class TestEnum extends Enum
-{
-    private const ONE = 1;
-    private const TWO = 2;
-
-    /**
-     * @var array
-     */
-    private static $name = [
-        self::ONE => 'yo',
-        self::TWO => 'Double yo',
-    ];
-
-    /**
-     * @var array
-     */
-    private static $identifier = [
-        self::ONE => 'uno',
-        self::TWO => 'duo',
-    ];
-
-    /**
-     * @var array
-     */
-    private static $description = [
-        self::ONE => 'This is a description for TestEnum::ONE',
-        self::TWO => 'This is a description for TestEnum::TWO',
-    ];
-
-    /**
-     * @var array
-     */
-    private static $descriptionTwo = [
-        self::ONE => 'This is two description for one',
-        self::TWO => 'This is two description for two',
-    ];
-}
-
-/**
  * @method static AnotherTestEnum one()
  */
 class AnotherTestEnum extends Enum
